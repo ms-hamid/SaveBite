6fff9a144f93ceba77b2dac25582d657a9aceabc
bastian <bastian160305@gmail.com>
2026-07-19 20:50:27 +0700

    changing auth login secure and samesite settings

--- backend-core/src/controllers/auth.controller.js
+++ backend-core/src/controllers/auth.controller.js
@@ -66,12 +66,10 @@ export async function login(req, res) {
 
         res.cookie("sb_access_token", token, {
             httpOnly: true,
-            secure: false, // Set to true in production with HTTPS
-            sameSite: "lax",
+            secure: true, 
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000,
         });
-
-        
 
         return res.status(200).json({
             token: token,
